9b29b45f8da393f9eadaa78fce951020bac702ba
Israelb <yb8200@gmail.com>
2017-11-30 08:39:21 +0200

    Refer to the private url

---
+++ src/public/sign-up/sign-up.component.js
@@ -0,0 +1,15 @@
+(function () {
+  "use strict";
+  angular.module('public').
+  component('signUp', {
+    templateUrl: 'src/public/sign-up/sign-up.html',
+    bindings: {
+      firstName: '<',
+      lastName: '<',
+      emailAddress: '<',
+      phoneNumber: '<',
+      favoriteDish: '<',
+    },
+    controller: signUpController
+  });
+})();
